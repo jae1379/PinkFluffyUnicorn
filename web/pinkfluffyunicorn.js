a55987b255fcb9cc1d4ba4f8fb114e211537cd6f
Instafluff <instaflufftv@gmail.com>
2019-03-14 00:22:01 -0700

    Physics Objects

--- web/pinkfluffyunicorn.js
+++ web/pinkfluffyunicorn.js
@@ -1,5 +1,7 @@
 var assetReference = {};
 var overlays = {};
+var objects = {};
+var objectSprite = {};
 
 function LoadAsset( name, path ) {
 	var fileExt = path.split('.').pop();
@@ -18,13 +20,14 @@ function LoadAsset( name, path ) {
 
 function AddOverlay( name, assetName, x, y ) {
 	try {
+		// TODO: Check that the overlay name doesn't already exist
 		if( name ) {
 			var sprite = new PIXI.Sprite(
 				assetReference[ assetName ]
 			);
 			sprite.x = x;
 			sprite.y = y;
-			app.stage.addChild( sprite );
+			groupOverlay.addChild( sprite );
 			overlays[ name ] = sprite;
 			return sprite;
 		}
@@ -37,24 +40,71 @@ function AddOverlay( name, assetName, x, y ) {
 
 function RemoveOverlay( name ) {
 	if( overlays[ name ] ) {
-		app.stage.removeChild( overlays[ name ] );
+		groupOverlay.removeChild( overlays[ name ] );
 		delete overlays[ name ];
 	}
 }
 
+function AddObject( name, options ) {
+	try {
+		// TODO: Check that the object name doesn't already exist
+		if( name ) {
+			if( Array.isArray( options ) ) {
+				// TODO: Add Composite Object Support
+				throw new Error( "Composite Objects Not Supported" );
+			}
+			var body = null;
+			switch( options.type.toLowerCase() ) {
+				case "circle":
+					body = Matter.Bodies.circle( options.x, options.y, options.radius );
+					break;
+				default:
+					throw new Error( "Unsupported Object Type", options.type );
+			}
+			var sprite = new PIXI.Sprite(
+				assetReference[ options.sprite ]
+			);
+			sprite.anchor.set( 0.5 );
+			sprite.x = body.position.x;
+			sprite.y = body.position.y;
+			sprite.rotation = body.angle;
+			objectSprite[ body.id ] = sprite;
+			groupWorld.addChild( sprite );
+			objects[ name ] = body;
+			Matter.World.add( physics.world, [ body ] );
+			return body;
+		}
+	}
+	catch( err ) {
+		console.log( "Failed to add overlay", err );
+	}
+	return null;
+}
+
+function RemoveObject( name ) {
+	if( objects[ name ] ) {
+		// app.stage.removeChild( objects[ name ] );
+		delete objects[ name ];
+	}
+}
+
 var app = undefined;
+var groupOverlay = undefined;
+var groupWorld = undefined;
 var opts = undefined;
+var physics = undefined;
 
 function createTheUnicorn( element, options ) {
 	try {
 		opts = options;
+		PIXI.utils.skipHello();
 		app = new PIXI.Application( {
-			width: opts.width,	// default: 800
+			width: opts.width,		// default: 800
 			height: opts.height,	// default: 600
 			antialias: true,		// default: false
-			transparent: opts.background === "transparent", // default: false
+			transparent: opts.background === "transparent",
 			backgroundColor: opts.background === "transparent" ? null : opts.background,
-			resolution: 1       // default: 1
+			resolution: 1       	// default: 1
 		} );
 		if( element instanceof Element || element instanceof HTMLDocument ) {
 			element.appendChild( app.view );
@@ -65,11 +115,26 @@ function createTheUnicorn( element, options ) {
 		else {
 			throw new Error( "Invalid Element Type" );
 		}
+		groupWorld = new PIXI.Container();
+		app.stage.addChild( groupWorld );
+		groupOverlay = new PIXI.Container();
+		app.stage.addChild( groupOverlay );
 		if( opts.channel ) {
 			ComfyJS.Init( opts.channel );
 			ComfyJS.onCommand = opts.onCommand;
 			ComfyJS.onChat = opts.onChat;
 		}
+		physics = Matter.Engine.create();
+		if( opts.screenWalls || typeof opts.screenWalls === "undefined" ) {
+			// Set Walls
+			Matter.World.add( physics.world, [
+				Matter.Bodies.rectangle( opts.width / 2, -50, opts.width, 100, { isStatic: true } ),
+				Matter.Bodies.rectangle( opts.width / 2, opts.height + 50, opts.width, 100, { isStatic: true } ),
+				Matter.Bodies.rectangle( -50, opts.height / 2, 100, opts.height, { isStatic: true } ),
+				Matter.Bodies.rectangle( opts.width + 50, opts.height / 2, 100, opts.height, { isStatic: true } )
+			] );
+		}
+		Matter.Engine.run( physics );
 		// Initialize the engine
 		opts.init();
 		window.requestAnimationFrame( updateTheUnicorn );
@@ -87,6 +152,15 @@ function updateTheUnicorn( timestamp ) {
 		if( prevStep < 0 ) {
 			prevStep = timestamp;
 		}
+		// Update the object sprites based on the world
+		physics.world.bodies.forEach( b => {
+			var sprite = objectSprite[ b.id ];
+			if( sprite ) {
+				sprite.x = b.position.x;
+				sprite.y = b.position.y;
+				sprite.rotation = b.angle;
+			}
+		});
 		opts.update( timestamp, timestamp - prevStep );
 		prevStep = timestamp;
 		window.requestAnimationFrame( updateTheUnicorn );
@@ -101,4 +175,9 @@ window.Unicorn = {
 	Load: LoadAsset,
 	AddOverlay: AddOverlay,
 	RemoveOverlay: RemoveOverlay,
+	AddObject: AddObject,
+	RemoveObject: RemoveObject,
+	Assets: assetReference,
+	Overlays: overlays,
+	Objects: objects
 };
