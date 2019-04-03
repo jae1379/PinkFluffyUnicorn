a74dde503a5d8f9b429602e5d071abc07afcdbbf
Instafluff <35773245+instafluff@users.noreply.github.com>
2019-04-03 15:27:10 -0700

    Webpack Works!

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const ConcatPlugin = require('webpack-concat-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'pinkfluffyunicorn.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    "mode": "development",
+    "entry": "./src/index.js",
+    "output": {
+        "path": __dirname+'/dist',
+        "filename": "ignorethis.js"
+    },
+    "devtool": "source-map",
+    plugins: [
+        new ConcatPlugin({
+            uglify: true,
+            sourceMap: true,
+            name: 'pinkfluffyunicorn',
+            fileName: 'pinkfluffyunicorn.min.js',
+            filesToConcat: ['./src/lib/matter.min.js','./src/lib/pixi.min.js','./src/lib/pixi-sound.js','./src/lib/comfy.min.js','./src/engine.js' ]
+        })
+    ]
 };
